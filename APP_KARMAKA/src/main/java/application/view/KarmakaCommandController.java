e1fe7f0498d6b7b0f60d44763093ee07d78b4762
Antoine <antoine.mazeau31@gmail.com>
2023-10-22 09:31:39 +0200

    printer

--- APP_KARMAKA/src/main/java/application/view/KarmakaCommandController.java
+++ APP_KARMAKA/src/main/java/application/view/KarmakaCommandController.java
@@ -6,10 +6,26 @@ public class KarmakaCommandController {
 
     public void displayGameStart() {
         System.out.println("========================= KARMAKA 2023 =========================");
-        System.out.println("Bienvenu sur le jeu de société Karmaka !");
+        this.print("Bienvenu sur le jeu de société Karmaka !");
     }
 
     public void beginDisplayOfTheGame() {
-        System.out.println("Début d'une partie ...");
+        this.print("Début d'une partie ...");
+    }
+
+    private void print(String _s){
+        for(char c: _s.toCharArray()){
+            System.out.print(c);
+            wait(20);
+        }
+        System.out.println();
+    }
+
+    private void wait(int ms) {
+        try{
+            Thread.sleep(ms);
+        } catch(InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
     }
 }
